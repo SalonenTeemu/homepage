19715b10d852e2cfde3f7afb04f4da7aea92836d
Teemu Salonen <teemutapani.salonen@gmail.com>
2025-02-22 18:54:46 +0200

    Update env variables

--- src/app/lib/dynamoDbClient.ts
+++ src/app/lib/dynamoDbClient.ts
@@ -7,8 +7,8 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({
 	region: process.env.AWS_REGION || "eu-north-1",
 	credentials: {
-		accessKeyId: process.env.AWS_ACCESS_KEY || "",
-		secretAccessKey: process.env.AWS_SECRET_KEY || "",
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID || "",
 	},
 });
 
