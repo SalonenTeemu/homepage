32561709ce088a241b57d939581a977eee508447
Teemu Salonen <teemutapani.salonen@gmail.com>
2026-06-03 17:51:57 +0300

    Update project packages

--- src/app/components/introduction/introduction.tsx
+++ src/app/components/introduction/introduction.tsx
@@ -65,7 +65,7 @@ export default function Introduction() {
 								</p>
 								<p className="pb-2 pl-0 pt-2">
 									<span className="text-lime-500 selection:text-slate-950">Latest update</span>:
-									23.03.2026
+									03.06.2026
 								</p>
 							</div>
 						</div>
